66658fc14da81e1f5dc84eb434b1a97848909fe0
Nanna Ketola <nannailona@gmail.com>
2024-10-30 11:36:08 +0200

    Add tests so that test coverage for varasto.py is 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,42 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ottaminen_enemman_kuin_varastossa_on(self):
+        self.varasto.lisaa_varastoon(5)
+        saatu_maara = self.varasto.ota_varastosta(7)
+
+        self.assertAlmostEqual(saatu_maara, 5)
+
+    def test_lisataan_liikaa(self):
+        self.varasto.lisaa_varastoon(15)
+        
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_lisataan_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(-5)
+        
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_negatiivinen_ottaminen(self):
+        saatu_maara = self.varasto.ota_varastosta(-5)
+        
+        self.assertAlmostEqual(saatu_maara, 0.0)
+    
+    def test_alku_saldo_negatiivinen(self):
+        self.varasto = Varasto(10, -5)
+        
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_alku_saldo_liian_suuri(self):
+        self.varasto = Varasto(10, 11)
+        
+        self.assertAlmostEqual(self.varasto.saldo, 10.0)
+
+    def test_alku_tilavuus_negatiivinen(self):
+        self.varasto = Varasto(-5)
+        
+        self.assertAlmostEqual(self.varasto.tilavuus, 0.0)
+    
+    def test_tulostus(self):
+        self.assertEqual(str(self.varasto) ,"saldo = 0, vielä tilaa 10") 
