cbba454c9676aca64df5c07ee82f3d98aaab2d1c
Nanna Ketola <nannailona@gmail.com>
2024-11-06 11:39:12 +0200

    Add pylint

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -47,33 +47,32 @@ class TestVarasto(unittest.TestCase):
 
     def test_lisataan_liikaa(self):
         self.varasto.lisaa_varastoon(15)
-        
+
         self.assertAlmostEqual(self.varasto.saldo, 10)
 
     def test_lisataan_negatiivinen_maara(self):
         self.varasto.lisaa_varastoon(-5)
-        
+
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_negatiivinen_ottaminen(self):
         saatu_maara = self.varasto.ota_varastosta(-5)
-        
+
         self.assertAlmostEqual(saatu_maara, 0.0)
-    
     def test_alku_saldo_negatiivinen(self):
         self.varasto = Varasto(10, -5)
-        
+
         self.assertAlmostEqual(self.varasto.saldo, 0.0)
 
     def test_alku_saldo_liian_suuri(self):
         self.varasto = Varasto(10, 11)
-        
+
         self.assertAlmostEqual(self.varasto.saldo, 10.0)
 
     def test_alku_tilavuus_negatiivinen(self):
         self.varasto = Varasto(-5)
-        
+
         self.assertAlmostEqual(self.varasto.tilavuus, 0.0)
-    
+
     def test_tulostus(self):
-        self.assertEqual(str(self.varasto) ,"saldo = 0, vielä tilaa 10") 
+        self.assertEqual(str(self.varasto) ,"saldo = 0, vielä tilaa 10")
